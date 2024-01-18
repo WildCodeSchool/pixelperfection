9795f257c48ebf43b6758d06ce04ab70717c3609
Pagot Marion <marion.pagot@gmail.com>
2024-01-18 15:52:23 +0100

    css

--- back-php/src/Form/QualityType.php
+++ back-php/src/Form/QualityType.php
@@ -11,23 +11,44 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class QualityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $choices = [
+            1 => '1',
+            2 => '2',
+            3 => '3',
+            4 => '4',
+            5 => '5',
+        ];
         $builder
             ->add('product', EntityType::class, [
                 'class' => Product::class,
                 'choice_label' => 'name',
                 'multiple' => true,
             ])
-            ->add('parfume')
-            ->add('texture')
-            ->add('application')
-            ->add('packaging_attractive')
-            ->add('packaging_convenient')
-            ->add('efficiency')
+            ->add('parfume', ChoiceType::class, [
+                'choices' => $choices,
+            ])
+            ->add('texture', ChoiceType::class, [
+                'choices' => $choices,
+            ])
+            ->add('application', ChoiceType::class, [
+                'choices' => $choices,
+            ])
+            ->add('packaging_attractive', ChoiceType::class, [
+                'choices' => $choices,
+            ])
+            ->add('packaging_convenient', ChoiceType::class, [
+                'choices' => $choices,
+            ])
+            ->add('efficiency', ChoiceType::class, [
+                'choices' => $choices,
+            ])
+
             ->add('allergy')
         ;
     }
